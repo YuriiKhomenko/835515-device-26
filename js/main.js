d432b6bf32409494b14fb0e097199a63a85ce24a
Yurii Khomenko <yuriikhomenko@MBP-Yurii.home>
2019-10-22 06:36:16 +0200

    Добавляет стилизацию футера

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 'use strict'
 
-var serviceBtns = document.querySelectorAll('.services__btn');
-var serviceList = document.querySelector('.services__list');
-var servicesInfoList = document.querySelectorAll('.services__info');
-var orders = ['Доставка', 'Гарантия', 'Кредит']
+// var serviceBtns = document.querySelectorAll('.services__btn');
+// var serviceList = document.querySelector('.services__list');
+// var servicesInfoList = document.querySelectorAll('.services__info');
+// var orders = ['Доставка', 'Гарантия', 'Кредит']
 
-serviceList.addEventListener('mousedown', function(e) {
-  var index = orders.indexOf(e.target.innerHTML);
-  for (var i = 0; i < serviceBtns.length; i++) {
-    if (serviceBtns[i].classList.length > 1) {
-      serviceBtns[i].classList.remove('services__btn--active');
-    }
-    servicesInfoList[i].style.display = 'none';
-    if (i == index) {
-      servicesInfoList[i].style.display = 'block';
-    }
-  }
-  if (e.target.nodeName == 'BUTTON') {
-    e.target.classList.add('services__btn--active');
-  }
-})
+// serviceList.addEventListener('mousedown', function(e) {
+//   var index = orders.indexOf(e.target.innerHTML);
+//   for (var i = 0; i < serviceBtns.length; i++) {
+//     if (serviceBtns[i].classList.length > 1) {
+//       serviceBtns[i].classList.remove('services__btn--active');
+//     }
+//     servicesInfoList[i].style.display = 'none';
+//     if (i == index) {
+//       servicesInfoList[i].style.display = 'block';
+//     }
+//   }
+//   if (e.target.nodeName == 'BUTTON') {
+//     e.target.classList.add('services__btn--active');
+//   }
+// })
 
 var sliderControlls = document.querySelectorAll('.slider-nav__btn');
 var sliders = document.querySelectorAll('.slider__item');
@@ -33,5 +33,21 @@ sliderControlls.forEach(function (button, index) {
     })
     button.className = 'slider-nav__btn slider-nav__btn--active';
     sliders[index].className = 'slider__item slider__item--' + parseInt(index + 1) + ' slider__item--active';
+  })
+})
+
+var servicesControlls = document.querySelectorAll('.services__btn');
+var servicesInfos = document.querySelectorAll('.services__info');
+
+servicesControlls.forEach(function (button, index) {
+  button.addEventListener('click', function () {
+    servicesControlls.forEach(function (button) {
+      button.classList.remove('services__btn--active');
+    })
+    servicesInfos.forEach(function (service) {
+      service.classList.remove('services__info--active')
+    })
+    button.classList.add('services__btn--active');
+    servicesInfos[index].classList.add('services__info--active');
   })
 })
